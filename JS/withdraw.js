f7f153ce99c12b3df191de473bee8ba01a5d6802
nabiursiddique <nabiursiddique01@gmail.com>
2022-08-18 20:04:15 +0600

    the alert box is added

--- JS/withdraw.js
+++ JS/withdraw.js
@@ -12,6 +12,13 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     //Now updating the value in the balance dashboard 
     //getting the balance field value
     const balanceFieldValue = getDashboardValue('balance-amount');
+
+    //If the balance field amount is less than withdraw amount then we will show a alert
+    if(balanceFieldValue < withdrawFieldValue){
+        alert("You don't have efficient balance to withdraw.");
+        return;
+    }
+    
     const newBalanceAmount = balanceFieldValue - withdrawFieldValue;
     //setting the new balance in the balance dashboard
     setTheUpdatedValue('balance-amount', newBalanceAmount);
